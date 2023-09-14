c3812d67d0ac95563bb095de9847c63bd3dd5e0b
Ariel Contreras <arielcontra@hotmail.com>
2023-09-14 19:44:51 -0300

    error handling in api request and text footer

--- src/components/adviceStore.js
+++ src/components/adviceStore.js
@@ -11,11 +11,15 @@ const useAdviceStore = create((set) => ({
         set({ loading: true, error: null })
         try {
             const response = await fetch('https://api.adviceslip.com/advice')
+            if (!response.ok) {
+                throw new Error(`HTTP Error: ${response.status}`)
+            }
             const data = await response.json()
             const { id, advice } = data.slip
             set({ id, advice, loading: false })
             console.log(15)
         } catch (error) {
+            console.error(error)
             set({ loading: false, error: 'Error fetching advice' })
         }
     }
